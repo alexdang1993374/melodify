9e0790f7ed6ca5cc5d6a44aa8be6481bb832c851
Alexander Dang <alexdang1993374@gmail.com>
2023-06-05 15:55:38 -0700

    fix: login

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,22 +1,29 @@
 "use client";
 
-import React from "react";
 import {
   useSessionContext,
   useSupabaseClient,
 } from "@supabase/auth-helpers-react";
-import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
-
-import Modal from "./Modal";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 import useAuthModal from "@/hooks/useAuthModal";
+import Modal from "./Modal";
 
 const AuthModal = () => {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
   const { session } = useSessionContext();
   const { onClose, isOpen } = useAuthModal();
+
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
 
   const onChange = (open: boolean) => {
     if (!open) {
