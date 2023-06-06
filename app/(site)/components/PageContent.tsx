710e9e6665b1715df09a4386dc5bbafbb52cab2c
Alexander Dang <alexdang1993374@gmail.com>
2023-06-06 15:59:58 -0700

    feat: player slider

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { ISong } from "@/types";
-import React from "react";
 
 interface IPageContent {
   songs: ISong[];
 }
 
 const PageContent = ({ songs }: IPageContent) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available.</div>;
   }
@@ -16,7 +18,11 @@ const PageContent = ({ songs }: IPageContent) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
       {songs.map((song) => (
-        <SongItem key={song.id} onClick={() => {}} data={song} />
+        <SongItem
+          key={song.id}
+          onClick={(id: string) => onPlay(id)}
+          data={song}
+        />
       ))}
     </div>
   );
